775fd2ffa0c269bb06692655f54367387e2dae24
Orlando <221299oaHG>
2022-02-27 21:49:14 -0600

    refactor: Se optimizo las llamadas a los headers y footers

--- index.php
+++ index.php
@@ -10,45 +10,8 @@
 </head>
 
 <body>
-  <!-- Cabecera Principal -->
-  <nav class="cabecera">
-    <div class="container">
-      <div class="cabecera-logo">
-        <a href="index.php" class="cabecera_logo-links">
-          <img src="assets/img/logo_nav.png" alt="logo abarrotes villa" width="100px" height="100px">
-          <p>ABARROTES VILLA</p>
-        </a>
-      </div>
-      <div class="cabecera-buscador">
-        <input type="text" name="" id="" placeholder=" Buscar producto...">
-      </div>
-      <div class="cabecera-derecha">
-        <div class="cabecera-login">
-          <a href="#">
-              <img src="assets/img/user-alt.png" alt="Inicio de sesión" width="30px" height="30px">
-          </a>  
-        </div>
-        <div class="cabecera-carrito">
-            <a href="#">
-                <img src="assets/img/shopping-cart.png" alt="Carrito" width="30px" height="30px">
-            </a>
-        </div>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Cabecera de enlaces -->
-  <nav class="nav-enlaces">
-    <div class="container">
-      <ul class="nav-enlaces-ul">
-        <li><a href="#">Cotiza en línea</a></li>
-        <li><a href="#">¿Quiénes somos?</a></li>
-        <li><a href="horario.php">Horario</a></li>
-        <li><a href="ubicacion.php">Ubicación</a></li>
-        <li><a href="contacto.php">Contacto</a></li>
-      </ul>
-    </div>
-  </nav>    
+  <!-- Cabecera -->
+  <?php require_once "templates/header.php" ?>
 
   <!-- Cuerpo de nuestra landing page -->
   <br><br>
@@ -121,25 +84,9 @@
       </div>    
     </div>
   </section>
-    
-  <!-- Pie de la página -->
-  <footer class="footer">
-    <h1 class="footer_titulo-redes">REDES SOCIALES</h1>
-    <div class="footer_redes">
-      <a href="# ">
-        <img src="assets/img/fb.png " alt=" " width="50px"/>
-      </a>
-      <a href="# ">
-        <img src="assets/img/wa.png " alt=" " width="50px"/>
-      </a>
-      <a href="#">
-        <img src="assets/img/ig.png " alt=" " width="50px"/>
-      </a>
-    </div>
-    <p>DERECHOS RESERVADOS POR EQUIPO 2</p>
-  </footer>
+  
+  <!-- Footer de la página -->
+  <?php require_once "templates/footer.php" ?>
 
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ templates/footer.php
@@ -0,0 +1,19 @@
+  <!-- Pie de la página -->
+  <footer class="footer">
+    <h1 class="footer_titulo-redes">REDES SOCIALES</h1>
+    <div class="footer_redes">
+      <a href="# ">
+        <img src="assets/img/fb.png " alt=" " width="50px"/>
+      </a>
+      <a href="# ">
+        <img src="assets/img/wa.png " alt=" " width="50px"/>
+      </a>
+      <a href="#">
+        <img src="assets/img/ig.png " alt=" " width="50px"/>
+      </a>
+    </div>
+    <p>DERECHOS RESERVADOS POR EQUIPO 2</p>
+  </footer>
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
